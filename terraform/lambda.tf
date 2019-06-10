dd07c5afcf9e22cc7b276caa1a0ed8bf40ff81fd
Ryan Zander <exide@hotmail.com>
2019-06-09 20:10:27 -0700

    Don't provide a Lambda package at creation.

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,6 @@ resource "aws_lambda_function" "get_item_by_name" {
   function_name = "get-item-by-name"
   runtime = "nodejs8.10"
   handler = "index.handler"
-  filename = "../dist/get-item-by-name.zip"
   timeout = 60
   role = aws_iam_role.lambda.arn
   tags = { MajorMUD_API = "" }
