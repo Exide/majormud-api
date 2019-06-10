2ebb601905d955f47e39c3118b42f2f3d59508b6
Ryan Zander <exide@hotmail.com>
2019-06-09 21:50:21 -0700

    Specify a regional API.

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,6 +1,9 @@
 resource "aws_api_gateway_rest_api" "majormud_api" {
   name = "MajorMUD API"
   description = "An HTTP REST interface for accessing versioned MajorMUD information."
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
 
 resource "aws_api_gateway_resource" "version" {
