80afc31766166521b8e69ff5b82945f57b778649
Exide <exide@hotmail.com>
2019-06-09 19:33:00 -0700

    Fixed a bug in the assume_role_policy value.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "lambda" {
   name = "majormud-api-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_role
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_execution" {
