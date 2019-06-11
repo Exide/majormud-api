bb999ed759713c55e0419186a48bb9db09cbe9a6
Exide <exide@hotmail.com>
2019-06-10 21:30:00 -0700

    Query DynamoDB for the requested item data.

--- src/get-item-by-name.js
+++ src/get-item-by-name.js
@@ -1,6 +1,27 @@
-module.exports = (event, context) => {
+const aws = require('aws-sdk');
+const dynamodb = new aws.DynamoDB();
+
+exports.handler = async (event) => {
+  const version = event.pathParameters.version;
+  const itemName = decodeURI(event.pathParameters.item_name);
+
+  const item = await lookupByName(itemName, version);
+  if (!item) return { statusCode: 404 };
+
   return {
     statusCode: 200,
-    body: 'OK'
-  }
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(item)
+  };
 };
+
+async function lookupByName(name, version) {
+  const parameters = {
+    TableName: "majormud-items",
+    Key: {
+      version: { S: version },
+      name: { S: name }
+    }
+  };
+  return dynamodb.getItem(parameters).promise();
+}
