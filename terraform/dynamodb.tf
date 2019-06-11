ca2cf559dc3ad89b14ffa364a24fab11d521c1cd
Exide <exide@hotmail.com>
2019-06-10 23:17:33 -0700

    Switched to using the 'id' as the hash key.

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,14 +1,14 @@
 resource "aws_dynamodb_table" "races" {
   name = "majormud-races"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 
   attribute {
@@ -23,15 +23,15 @@ resource "aws_dynamodb_table" "races" {
 
 resource "aws_dynamodb_table" "classes" {
   name = "majormud-classes"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 
   attribute {
@@ -46,14 +46,14 @@ resource "aws_dynamodb_table" "classes" {
 
 resource "aws_dynamodb_table" "rooms" {
   name = "majormud-rooms"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
+    name = "id"
     type = "S"
   }
 
@@ -69,15 +69,15 @@ resource "aws_dynamodb_table" "rooms" {
 
 resource "aws_dynamodb_table" "npcs" {
   name = "majormud-npcs"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 
   attribute {
@@ -92,15 +92,15 @@ resource "aws_dynamodb_table" "npcs" {
 
 resource "aws_dynamodb_table" "items" {
   name = "majormud-items"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 
   attribute {
@@ -116,15 +116,15 @@ resource "aws_dynamodb_table" "items" {
 
 resource "aws_dynamodb_table" "spells" {
   name = "majormud-spells"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 
   attribute {
@@ -139,15 +139,15 @@ resource "aws_dynamodb_table" "spells" {
 
 resource "aws_dynamodb_table" "shops" {
   name = "majormud-shops"
-  hash_key = "hash"
+  hash_key = "id"
   range_key = "version"
 
   write_capacity = 25
   read_capacity = 25
 
   attribute {
-    name = "hash"
-    type = "S"
+    name = "id"
+    type = "N"
   }
 
   attribute {
