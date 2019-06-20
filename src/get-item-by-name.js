257328077771d57d03fa8c7bce42fe9bff4012fc
Exide <exide@hotmail.com>
2019-06-19 18:10:24 -0700

    Return 400 on invalid input.

--- src/get-item-by-name.js
+++ src/get-item-by-name.js
@@ -2,6 +2,10 @@ const aws = require('aws-sdk');
 const dbClient = new aws.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
+  if (isInvalidInput(event)) {
+    return { statusCode: 400 };
+  }
+
   const version = event.pathParameters.version;
   const itemName = decodeURI(event.pathParameters.item_name);
   const items = await getItemsByName(itemName, version);
@@ -34,4 +38,12 @@ async function getItemsByName(name, version) {
   };
   const { Items } = await dbClient.query(parameters).promise();
   return Items;
+}
+
+function isInvalidInput(event) {
+  const noInput = !event;
+  const emptyInput= !event.pathParameters;
+  const noVersion = !event.pathParameters.version;
+  const noItemName = !event.pathParameters.item_name;
+  return noInput || emptyInput || noVersion || noItemName;
 }
