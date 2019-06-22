2b075d050d1f6d1849ed189a9eaa59a081ae9795
Exide <exide@hotmail.com>
2019-06-22 10:33:40 -0700

    Moved common API code to the main API Gateway config.

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -5,3 +5,15 @@ resource "aws_api_gateway_rest_api" "majormud_api" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_resource" "v" {
+  rest_api_id = aws_api_gateway_rest_api.majormud_api.id
+  parent_id = aws_api_gateway_rest_api.majormud_api.root_resource_id
+  path_part = "v"
+}
+
+resource "aws_api_gateway_resource" "version" {
+  rest_api_id = aws_api_gateway_rest_api.majormud_api.id
+  parent_id = aws_api_gateway_resource.v.id
+  path_part = "{version}"
+}
--- terraform/get-items-by-name-api.tf
+++ terraform/get-items-by-name-api.tf
@@ -1,20 +1,8 @@
 // api.majormud.io/v/1.11p/items/:search
 
-resource "aws_api_gateway_resource" "version" {
-  rest_api_id = aws_api_gateway_rest_api.majormud_api.id
-  parent_id = aws_api_gateway_rest_api.majormud_api.root_resource_id
-  path_part = "v"
-}
-
-resource "aws_api_gateway_resource" "version_value" {
-  rest_api_id = aws_api_gateway_rest_api.majormud_api.id
-  parent_id = aws_api_gateway_resource.version.id
-  path_part = "{version}"
-}
-
 resource "aws_api_gateway_resource" "items" {
   rest_api_id = aws_api_gateway_rest_api.majormud_api.id
-  parent_id = aws_api_gateway_resource.version_value.id
+  parent_id = aws_api_gateway_resource.version.id
   path_part = "items"
 }
 
