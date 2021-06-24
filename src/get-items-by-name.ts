bb330d16de3eee448569c0416aee5c73c3a4e531
Ryan Zander <exide@hotmail.com>
2021-06-23 18:38:24 -0700

    Switched to a filtered scan to allow searching anywhere in an item's name.

--- src/get-items-by-name.ts
+++ src/get-items-by-name.ts
@@ -25,7 +25,7 @@ async function getItemsByName(name: string, version: string): Promise<MajorMUDIt
   const parameters = {
     TableName: 'majormud-items',
     IndexName: 'name',
-    KeyConditionExpression: '#name = :name and #version = :version',
+    FilterExpression: 'contains(#name, :name) and #version = :version',
     ExpressionAttributeNames: {
       '#name': 'name',
       '#version': 'version'
@@ -37,7 +37,7 @@ async function getItemsByName(name: string, version: string): Promise<MajorMUDIt
   };
 
   const dbClient = new DynamoDB.DocumentClient();
-  const result = await dbClient.query(parameters).promise();
+  const result = await dbClient.scan(parameters).promise();
   if (result.Items === undefined) return [];
 
   return result.Items.map(i => i as unknown as MajorMUDItem);
