05de9a2b1fd739b11c3d954771f5674d2e0c8470
Ryan Zander <exide@hotmail.com>
2021-06-25 07:08:55 -0700

    Added TODOs to resolve the disconnect between deployed infrastructure and Terraform.

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -8,3 +8,9 @@ provider "aws" {
 }
 
 data "aws_caller_identity" "current" {}
+
+// todo: save state to s3 and the lock to dynamodb
+// todo: link existing infrastructure to state
+// todo: resolve updates in terraform that haven't been synchronized
+// todo: build new infrastructure that only exists in terraform currently (index-api.tf)
+// todo: add existing infrastructure that doesnt exist in terraform currently (Route53)
