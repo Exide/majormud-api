0fb7d62d6cce910a98f07167fc2519c8c6d3e388
Ryan Zander <exide@hotmail.com>
2021-08-11 21:55:56 -0700

    Added a bunch of Gulp tasks to provide a lot of easy to use options.

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,10 @@ const path = require('path');
 const aws = require('aws-sdk');
 const mergeStream = require('merge-stream');
 const fs = require('fs').promises;
+const tsConfig = require('./tsconfig.json');
 
 const buildDir = path.resolve('build');
 const distDir = path.resolve('dist');
-
-const tsProject = typescript.createProject('tsconfig.json');
 
 async function cleanByFunctionName(functionName) {
   const artifacts = [ `build/${functionName}`, `dist/${functionName}.zip` ];
@@ -26,10 +25,10 @@ async function compileByFunctionName(functionName) {
     const functionCode = src(functionFile)
       .pipe(rename('index.ts'));
 
-    const sharedCode = src('src/helpers/**/*.ts');
+    const sharedCode = src('src/helpers/**/*.ts', { base: 'src/' });
 
     mergeStream(functionCode, sharedCode)
-      .pipe(tsProject())
+      .pipe(typescript(tsConfig.compilerOptions))
       .js
       .pipe(dest(functionBuildDir))
       .on('end', resolve)
@@ -65,7 +64,7 @@ function compressByFunctionName(functionName) {
 async function uploadByFunctionName(functionName) {
   const filename = path.resolve(distDir, `${functionName}.zip`);
   const parameters = {
-    FunctionName: functionName,
+    FunctionName: `majormud-api-${functionName}`,
     ZipFile: await fs.readFile(filename)
   };
 
@@ -75,55 +74,169 @@ async function uploadByFunctionName(functionName) {
   console.info(result);
 }
 
-//
+// ****************************************************************************
 //  Public Tasks
-//
+// ****************************************************************************
 
+// ****************************************************************************
+//  get-index
+// ****************************************************************************
+
+task('clean-get-index', series(
+  async function clean() { return cleanByFunctionName('get-index') }
+));
+task('build-get-index', series(
+  async function clean() { return cleanByFunctionName('get-index') },
+  async function compile() { return compileByFunctionName('get-index') },
+  async function getDependencies() { return getDependenciesByFunctionName('get-index') }
+));
+task('package-get-index', series(
+  async function compress() { return compressByFunctionName('get-index') }
+));
+task('upload-get-index', series(
+  async function upload() { return uploadByFunctionName('get-index') }
+));
 task('get-index', series(
-  async function clean() { return cleanByFunctionName('index') },
-  async function compile() { return compileByFunctionName('index') },
-  async function getDependencies() { return getDependenciesByFunctionName('index') },
-  async function compress() { return compressByFunctionName('index') },
-  async function upload() { return uploadByFunctionName('index') }
+  'build-get-index',
+  'package-get-index',
+  'upload-get-index'
 ));
 
-task('get-versions', series(
+// ****************************************************************************
+//  get-versions
+// ****************************************************************************
+
+task('clean-get-versions', series(
+  async function clean() { return cleanByFunctionName('get-versions') }
+));
+task('build-get-versions', series(
   async function clean() { return cleanByFunctionName('get-versions') },
   async function compile() { return compileByFunctionName('get-versions') },
-  async function getDependencies() { return getDependenciesByFunctionName('get-versions') },
-  async function compress() { return compressByFunctionName('get-versions') },
+  async function getDependencies() { return getDependenciesByFunctionName('get-versions') }
+
+));
+task('package-get-versions', series(
+  async function compress() { return compressByFunctionName('get-versions') }
+));
+task('upload-get-versions', series(
   async function upload() { return uploadByFunctionName('get-versions') }
 ));
+task('get-versions', series(
+  'build-get-versions',
+  'package-get-versions',
+  'upload-get-versions'
+));
 
-task('get-version-by-name', series(
+// ****************************************************************************
+//  get-version-by-name
+// ****************************************************************************
+
+task('clean-get-version-by-name', series(
+  async function clean() { return cleanByFunctionName('get-version-by-name') }
+));
+task('build-get-version-by-name', series(
   async function clean() { return cleanByFunctionName('get-version-by-name') },
   async function compile() { return compileByFunctionName('get-version-by-name') },
-  async function getDependencies() { return getDependenciesByFunctionName('get-version-by-name') },
-  async function compress() { return compressByFunctionName('get-version-by-name') },
+  async function getDependencies() { return getDependenciesByFunctionName('get-version-by-name') }
+));
+task('package-get-version-by-name', series(
+  async function compress() { return compressByFunctionName('get-version-by-name') }
+));
+task('upload-get-version-by-name', series(
   async function upload() { return uploadByFunctionName('get-version-by-name') }
 ));
+task('get-version-by-name', series(
+  'build-get-version-by-name',
+  'package-get-version-by-name',
+  'upload-get-version-by-name'
+));
 
-task('get-items', series(
+// ****************************************************************************
+//  get-items
+// ****************************************************************************
+
+task('clean-get-items', series(
+  async function clean() { return cleanByFunctionName('get-items') }
+));
+task('build-get-items', series(
   async function clean() { return cleanByFunctionName('get-items') },
   async function compile() { return compileByFunctionName('get-items') },
-  async function getDependencies() { return getDependenciesByFunctionName('get-items') },
-  async function compress() { return compressByFunctionName('get-items') },
+  async function getDependencies() { return getDependenciesByFunctionName('get-items') }
+));
+task('package-get-items', series(
+  async function compress() { return compressByFunctionName('get-items') }
+));
+task('upload-get-items', series(
   async function upload() { return uploadByFunctionName('get-items') }
 ));
-
-task('get-item-by-id', series(
-  async function clean() { return cleanByFunctionName('get-item-by-id') },
-  async function compile() { return compileByFunctionName('get-item-by-id') },
-  async function getDependencies() { return getDependenciesByFunctionName('get-item-by-id') },
-  async function compress() { return compressByFunctionName('get-item-by-id') },
-  async function upload() { return uploadByFunctionName('get-item-by-id') }
+task('get-items', series(
+  'build-get-items',
+  'package-get-items',
+  'upload-get-items'
 ));
 
-// clean, build, package, and upload everything
-task('default', parallel(
-  'get-index',
-  'get-versions',
-  'get-version-by-name',
-  'get-items',
-  'get-item-by-id'
+// ****************************************************************************
+//  get-item-by-id
+// ****************************************************************************
+
+task('clean-get-item-by-id', series(
+  async function clean() { return cleanByFunctionName('get-item-by-id') }
+));
+task('build-get-item-by-id', series(
+  async function clean() { return cleanByFunctionName('get-item-by-id') },
+  async function compile() { return compileByFunctionName('get-item-by-id') },
+  async function getDependencies() { return getDependenciesByFunctionName('get-item-by-id') }
+));
+task('package-get-item-by-id', series(
+  async function compress() { return compressByFunctionName('get-item-by-id') }
+));
+task('upload-get-item-by-id', series(
+    async function upload() { return uploadByFunctionName('get-item-by-id') }
+));
+task('get-item-by-id', series(
+  'build-get-item-by-id',
+  'package-get-item-by-id',
+  'upload-get-item-by-id'
+));
+
+// ****************************************************************************
+//  Aggregate Tasks
+// ****************************************************************************
+
+task('clean', parallel(
+  'clean-get-index',
+  'clean-get-versions',
+  'clean-get-version-by-name',
+  'clean-get-items',
+  'clean-get-item-by-id'
+));
+
+task('build', parallel(
+  'build-get-index',
+  'build-get-versions',
+  'build-get-version-by-name',
+  'build-get-items',
+  'build-get-item-by-id'
+));
+
+task('package', parallel(
+  'package-get-index',
+  'package-get-versions',
+  'package-get-version-by-name',
+  'package-get-items',
+  'package-get-item-by-id'
+));
+
+task('upload', parallel(
+  'upload-get-index',
+  'upload-get-versions',
+  'upload-get-version-by-name',
+  'upload-get-items',
+  'upload-get-item-by-id'
+));
+
+task('default', series(
+  'build',
+  'package',
+  'upload'
 ));
